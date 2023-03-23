efd257386684b95125032028ac61bc38cc791652
sanix-darker <s4nixd@gmail.com>
2023-03-23 22:17:11 +0100

    feat: updates on cello and clib installer

--- cello_install.sh
+++ cello_install.sh
@@ -1,13 +1,19 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
-echo "[+] ----------------------------"
+WHERE_I_WAS="$PWD"
+
 echo "[+] Cello installation started !"
+
 apt-get update
 apt-get install zip unzip wget build-essential gcc
-wget http://libcello.org/static/libCello-2.1.0.tar.gz
 
+mkdir /tmp/cello
+cd /tmp/cello
+
+wget http://libcello.org/static/libCello-2.1.0.tar.gz
 tar -xzf libCello-2.1.0.tar.gz
-cd libCello-2.1.0
+
+cd ./libCello-2.1.0
 
 make
 make check
@@ -17,5 +23,10 @@ cd ../
 # We delete the tar and the directory
 rm -rf libCello-2.1.0.tar.gz libCello-2.1.0
 
-echo "[+] Cello have been installed successfully !"
-echo "[+] ----------------------------------------"
+cd $WHERE_I_WAS
+
+if [ $? == 0 ]; then
+    echo "[+] Cello have been installed successfully !"
+else
+    echo "[x] Cello failed to install, something went wrong !"
+fi
--- clib_install.sh
+++ clib_install.sh
@@ -1,8 +1,19 @@
-echo "[+] ----------------------------"
+#!/bin/bash
+
+WHERE_I_WAS="$PWD"
+
 echo "[+] Clib installation started !"
+
 git clone https://github.com/clibs/clib.git /tmp/clib
 cd /tmp/clib
+
 make
-sudo make install
-echo "[+] Clib have been installed successfully !"
-echo "[+] ----------------------------------------"
+make install
+
+cd $WHERE_I_WAS
+
+if [ $? == 0 ]; then
+    echo "[+] Clib have been installed successfully !"
+else
+    echo "[x] Clib failed to install, something went wrong !"
+fi
